b2f8f0082c313bb5785e173e32c6b242d7fad299
Dilbrent <dilbrent@gmail.com>
2018-07-25 13:37:46 -0700

    Initial changes to build CPU version

---
+++ fix_hdf5_link.sh
@@ -0,0 +1,4 @@
+sudo ln -s /usr/lib/x86_64-linux-gnu/libhdf5_serial.so.100.0.1 /usr/lib/x86_64-linux-gnu/libhdf5.so
+sudo ln -s /usr/lib/x86_64-linux-gnu/libhdf5_serial_hl.so.100.0.0 /usr/lib/x86_64-linux-gnu/libhdf5_hl.so
+
+#sudo apt install libopencv-apps-dev
